540295f66e569b67ebbc9653f855c1c64bffcbb6
AVIV LEVI <60aviv60@gmail.com>
2023-06-10 17:32:18 +0300

    Add PrometheusResult tests

--- pypromql/result/prometheus_result.py
+++ pypromql/result/prometheus_result.py
@@ -1,5 +1,5 @@
 from typing import List
-from pypromql.query.prometheus_query import Query
+from pypromql.query.prometheus_query import Query, QueryBuilder
 from pypromql.result.prometheus_metric import Metric
 from pypromql.connection import PrometheusConnection
 
@@ -16,13 +16,16 @@ class PrometheusResult:
             result = prometheus_api_response_data['result']
             self._load_metrics_from_api_response(result)
         else:
-            pass
+            self.error_type = prometheus_api_response['errorType']
+            self.error = prometheus_api_response['error']
+        if 'warnings' in prometheus_api_response:
+            self.warnings = prometheus_api_response['warnings']
 
     def _load_metrics_from_api_response(self, response_metrics_list: List[dict]):
         for metric in response_metrics_list:
-            self.metrics.append(Metric(prometheus_api_metric_response=metric))
+            self.metrics.append(Metric.from_prometheus_api_metric_response(prometheus_api_metric_response=metric))
 
     @classmethod
-    def from_query(cls, query: Query, prometheus_connection: PrometheusConnection):
-        prometheus_api_response = prometheus_connection.execute_query(query=str(query))
+    def from_query_builder(cls, query_builder: QueryBuilder, prometheus_connection: PrometheusConnection):
+        prometheus_api_response = prometheus_connection.execute_query(query=query_builder.to_promql())
         return cls(prometheus_api_response)
