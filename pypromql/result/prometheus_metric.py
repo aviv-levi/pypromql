b293ff9d08437be9cb518714dd1a0548e627a251
Dr. Su Sa <sudarsun@buddi.ai>
2025-03-28 17:52:09 +0530

    the attribute has changed from 'value' to 'values' moreover, 'values' carry a list of tuples @<timestamp, value>. Therefore, it requires to scan through the list to collect the tuple items.

--- pypromql/result/prometheus_metric.py
+++ pypromql/result/prometheus_metric.py
@@ -15,6 +15,11 @@ class Metric:
     @classmethod
     def from_prometheus_api_metric_response(cls, prometheus_api_metric_response: dict):
         labels = prometheus_api_metric_response['metric']
-        timestamp = prometheus_api_metric_response['value'][0]
-        value = float(prometheus_api_metric_response['value'][1])
+        # the attribute has changed from 'value' to 'values'
+        # moreover, 'values' carry a list of tuples @<timestamp, value>.
+        # Therefore, it requires to scan through the list to collect the tuple items.
+        timestamp = [value[0] for value in prometheus_api_metric_response['values']]
+        value = [float(value[1]) for value in prometheus_api_metric_response['values']]
+        #timestamp = prometheus_api_metric_response['value'][0]
+        #value = float(prometheus_api_metric_response['value'][1])
         return cls(timestamp=timestamp, value=value, labels=labels)
